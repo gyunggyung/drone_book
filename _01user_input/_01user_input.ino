f494968aafadd85c543f7d689aba948e1cde9bbe
newhiwoong <newhiwoong@gmail.com>
2017-04-12 19:48:55 +0900

    error pass

---
+++ _01user_input/_01user_input.ino
@@ -0,0 +1,13 @@
+void setup() {
+    Serial1.begin(115200);
+    Serial.begin(115200);
+}
+
+void loop() {
+  uint8_t userInput;
+  if(Serial1.available() > 0) {
+    userInput = Serial1.read();
+    
+    Serial.println(char)userInput);
+  }
+}
